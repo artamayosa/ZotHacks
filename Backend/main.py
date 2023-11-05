c6445972556a997c0df312f445597bc82c62cecc
Arvin Tamayosa <artamayosa@gmail.com>
2023-11-04 18:19:58 -0700

    removed unneeded helper

--- Backend/main.py
+++ Backend/main.py
@@ -73,12 +73,6 @@ async def getRooms(start_time: Annotated[str, Form()]):
 
 
 
-def time_in_range(start, end, x):
-    """Return true if x is in the range [start, end]"""
-    if start <= end:
-        return start <= x <= end
-    else:
-        return start <= x or x <= end
   
 
 @app.get('/times')
