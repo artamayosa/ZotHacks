9145539f6c004a30ebbd5dfc1d951858eaec2c37
Arvin Tamayosa <artamayosa@gmail.com>
2023-11-04 19:39:19 -0700

    connected log in and rooms page and api

--- Frontend/rooms.js
+++ Frontend/rooms.js
@@ -1,75 +1,9 @@
 document.addEventListener('DOMContentLoaded', onLoad)
 
-const locations = [
-    {
-        name: "CSL",
-        rooms: [
-            "Room 1",
-            "Room 2",
-            "Room 3"
-        ]
-    },
-    {
-        name: "Science Library",
-        rooms: [
-            "Room 1",
-            "Room 2",
-            "Room 3"
-        ]
-    },
-    {
-        name: "Langston Library",
-        rooms: [
-            "Room 1",
-            "Room 2",
-            "Room 3"
-        ]
-    },
-    {
-        name: "Langston Library",
-        rooms: [
-            "Room 1",
-            "Room 2",
-            "Room 3"
-        ]
-    },
-    {
-        name: "Langston Library",
-        rooms: [
-            "Room 1",
-            "Room 2",
-            "Room 3"
-        ]
-    },
-    {
-        name: "Langston Library",
-        rooms: [
-            "Room 1",
-            "Room 2",
-            "Room 3"
-        ]
-    },
-    {
-        name: "Langston Library",
-        rooms: [
-            "Room 1",
-            "Room 2",
-            "Room 3"
-        ]
-    },
-    {
-        name: "Langston Library",
-        rooms: [
-            "Room 1",
-            "Room 2",
-            "Room 3"
-        ]
-    }
-]
 
 async function onLoad() {
-    const actualLocations = await fetch('/locations').then(response => response.json()).catch(() => [])
-
+    const locations = await fetch('http://localhost:8000/locations', {method:'GET'}).then(response => response.json()).catch(() => [])
+    console.log(locations)
     // const emoji = await fetch('https://emojihub.yurace.pro/api/random')
     // const data = await emoji.json()
     // console.log(emoji, data)
@@ -102,7 +36,7 @@ async function onLoad() {
             button.addEventListener('click', (event) => {
                 event.preventDefault()
                 const room = event.target.textContent
-                window.location.href = `/scheduling.html?room=${room}&location=${location.name}`
+                window.location.href = `/static/scheduling.html?room=${room}&location=${location.name}`
 
             })
 
